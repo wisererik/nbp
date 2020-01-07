e5001de11c8a980613fee4bea7e885ac6cce6b73
sushanthakumar <sushanthakumar@huawei.com>
2020-01-07 18:49:25 +0530

    Node expand changes

--- csi/plugins/block/node.go
+++ csi/plugins/block/node.go
@@ -15,12 +15,17 @@
 package block
 
 import (
+	"fmt"
 	csi "github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/golang/glog"
 	"github.com/opensds/nbp/csi/common"
+	"github.com/opensds/nbp/csi/util"
+	"github.com/opensds/opensds/contrib/connector"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	"strings"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -115,10 +120,38 @@ func (p *Plugin) NodeExpandVolume(
 	// This funcationality to be filled
 
 	// Check REQUIRED field
-	glog.V(5).Info("start to node expand volume, volume_id: " + req.VolumeId)
 	defer glog.V(5).Info("end to node expand volume")
+	volumeID := req.GetVolumeId()
+	if len(volumeID) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume ID not provided")
+	}
 
-	return nil, status.Error(codes.Unimplemented, "")
+	capacityBytes := common.GetSize(req.GetCapacityRange())
+	volSizeBytes := capacityBytes * util.GiB
+
+	args := []string{"-o", "source", "--noheadings", "--target", req.GetVolumePath()}
+	glog.V(5).Infof("end to node expand volume, args: %v", args)
+	output, err := connector.ExecCmd("findmnt", args...)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not determine device path: %v", err)
+	}
+
+	glog.V(5).Infof("end to node expand volume, mounted: %v", output)
+	devicePath := strings.TrimSpace(string(output))
+	if len(devicePath) == 0 {
+		return nil, status.Errorf(codes.Internal, "Could not get valid device for mount path: %q", req.GetVolumePath())
+	}
+	glog.V(5).Infof("end to node expand volume, devicePath: %v", devicePath)
+
+	var result bool
+	if result, err = resize(devicePath, req.GetVolumePath()); err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not resize volume %q (%q):  %v", volumeID, devicePath, err)
+	}
+
+	glog.V(5).Infof("end to node expand volume, volume_size: %v, resizeresult: %v", volSizeBytes, result)
+	return &csi.NodeExpandVolumeResponse{
+		CapacityBytes: volSizeBytes,
+	}, nil
 }
 
 // NodeGetInfo gets information on a node
@@ -146,4 +179,96 @@ func (p *Plugin) NodeGetVolumeStats(
 	*csi.NodeGetVolumeStatsResponse, error) {
 
 	return common.NodeGetVolumeStats(ctx, req)
+}
+
+
+// Resize perform resize of file system
+func resize(devicePath string, deviceMountPath string) (bool, error) {
+	format, err := connector.GetFSType(devicePath)
+
+	if err != nil {
+		formatErr := fmt.Errorf("ResizeFS.Resize - error checking format for device %s: %v", devicePath, err)
+		return false, formatErr
+	}
+
+	// If disk has no format, there is no need to resize the disk because mkfs.*
+	// by default will use whole disk anyways.
+	if format == "" {
+		return false, nil
+	}
+
+	glog.V(5).Infof("ResizeFS.Resize - format: %v", format)
+	switch format {
+	case "ext3", "ext4":
+		return extResize(devicePath)
+	case "xfs":
+		return xfsResize(deviceMountPath)
+	}
+	return false, fmt.Errorf("ResizeFS.Resize - resize of format %s is not supported for device %s mounted at %s", format, devicePath, deviceMountPath)
+}
+
+func  extResize(devicePath string) (bool, error) {
+
+	Para := fmt.Sprintf("lsscsi | grep %s", devicePath)
+	output, err := connector.ExecCmd("/bin/bash", "-c", Para)
+	if err != nil {
+		glog.V(5).Infof("Failed to execute lsscsi for device output : %v, err : %v", output, err)
+		glog.Error(err.Error())
+		return false, err
+	}
+
+	//Output looks like : [4:0:0:1]    disk    IET      VIRTUAL-DISK     0001  /dev/sdb
+	// Need to parse and get host identifier (4 in above case)
+	glog.V(5).Infof("end to node expand volume, lsscsi: %v", output)
+	hostId := strings.Split(output, " ")[0]
+	hostId = strings.Split(hostId, ":")[0]
+	hostId = strings.Split(hostId, "[")[1]
+	glog.V(5).Infof("end to node expand volume, hostId: %v", hostId)
+
+	scanCommandPara := "'- - -' > /sys/class/scsi_host/host" + hostId + "/scan"
+
+	output, err = connector.ExecCmd("echo", scanCommandPara)
+	if err != nil {
+		glog.V(5).Infof("Failed to execute scan command for device output : %v, err : %v", output, err)
+		glog.Error(err.Error())
+		return false, err
+	}
+
+	deviceIndex := strings.Split(devicePath, "/dev/")[1]
+	glog.V(5).Infof("end to node expand volume, deviceIndex: %v", deviceIndex)
+
+	reScanCommandPara :=  "'1' > /sys/block/" + deviceIndex + "/device/rescan"
+	output, err = connector.ExecCmd("echo",reScanCommandPara)
+	if err != nil {
+		glog.V(5).Infof("Failed to execute rescan command for device output : %v, err : %v", output, err)
+		glog.Error(err.Error())
+		return false, err
+	}
+
+	glog.V(5).Infof("end to node expand volume, reScanCommand: %v", reScanCommandPara)
+
+	output, err = connector.ExecCmd("resize2fs", devicePath)
+	if err != nil {
+		glog.V(5).Infof("Failed to execute resize command for device path : %v", devicePath)
+		glog.Error(err.Error())
+		return false, err
+	}
+
+	//resizeError := fmt.Errorf("resize of device %s failed: %v. resize2fs output: %s", devicePath, err, string(output))
+	glog.V(5).Infof("Resize success for device path : %v", devicePath)
+	return true, nil
+
+}
+
+func xfsResize(deviceMountPath string) (bool, error) {
+	args := []string{"-d", deviceMountPath}
+	output, err := connector.ExecCmd("xfs_growfs", args...)
+
+	if err == nil {
+		klog.V(2).Infof("Device %s resized successfully", deviceMountPath)
+		return true, nil
+	}
+
+	resizeError := fmt.Errorf("resize of device %s failed: %v. xfs_growfs output: %s", deviceMountPath, err, string(output))
+	return false, resizeError
 }
